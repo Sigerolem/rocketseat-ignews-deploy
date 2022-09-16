3b6005ae1d80f766d631b37942958862df95e780
Régis Oliveira <regisoliveiramelo@gmail.com>
2022-09-16 18:03:15 -0300

    fix build error prismic

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -3,6 +3,7 @@ import Stripe from 'stripe'
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
   {
+    // @ts-ignore stripe-version-2022-08-01
     apiVersion: '2020-08-27',
     appInfo: {
       name: 'IgNews',
